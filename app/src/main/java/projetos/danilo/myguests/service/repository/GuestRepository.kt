f3ede67a1fa0be6a697383d367f06e1d04a89454
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-06-24 12:11:50 -0300

    getAll guest from db

--- app/src/main/java/projetos/danilo/myguests/service/repository/GuestRepository.kt
+++ app/src/main/java/projetos/danilo/myguests/service/repository/GuestRepository.kt
@@ -35,10 +35,10 @@ class GuestRepository private constructor(context: Context) {
             )
 
             val contentValues = ContentValues()
-            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest?.name)
             contentValues.put(
                 DataBaseConstants.GUEST.COLUMNS.PRESENCE,
-                guest.presence
+                guest?.presence
             ) //banco trata boolean para inteiro 0 - falso
 
             val whereClause = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
@@ -68,7 +68,40 @@ class GuestRepository private constructor(context: Context) {
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
-        return list
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+
+            //Colunas de dados que queremos retornar dos usuários
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            //cursor que retorna os dados
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection, null, null, null, null, null
+            )
+
+            if(cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence = (cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+
+                    val guest = GuestModel(id, name, presence)
+                    list.add(guest)
+                }
+
+            }
+
+            cursor?.close()
+            list
+        } catch (e: Exception) {
+            list
+        }
     }
 
     fun getPresent(): List<GuestModel> {
